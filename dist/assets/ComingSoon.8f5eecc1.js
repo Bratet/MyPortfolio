e4d4117117e88cada21fdcab19f30a151b6d8219
Bratet <ahmedmrabet.002@gmail.com>
2022-11-19 18:33:37 +0100

    adding dist

---
+++ dist/assets/ComingSoon.8f5eecc1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.5f0506cd.js";function i(){return o("div",{children:"coming soon page"})}export{i as default};
---
+++ dist/assets/Page500.28475bb2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.5f0506cd.js";function a(){return e("div",{children:"500 page"})}export{a as default};
