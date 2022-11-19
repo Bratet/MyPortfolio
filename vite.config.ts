19f74f180f7429ea524c93a93ad7270cadbe9da3
Bratet <ahmedmrabet.002@gmail.com>
2022-11-19 18:09:27 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgLoader from "vite-svg-loader";
 import svgr from "@honkhonk/vite-plugin-svgr";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader(), svgr()],
-  base: "./",
+  plugins: [vue()],
+  base: "/MyPortfolio/",
 });
