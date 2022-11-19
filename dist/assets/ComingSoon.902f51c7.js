72abef9245ae0d3e47f358f3f314b6119263a9fe
Bratet <ahmedmrabet.002@gmail.com>
2022-11-19 19:07:14 +0100

    new setup

--- dist/assets/ComingSoon.902f51c7.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.8413ccdb.js";function i(){return o("div",{children:"coming soon page"})}export{i as default};
--- dist/assets/Page500.3227d3c1.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.8413ccdb.js";function a(){return e("div",{children:"500 page"})}export{a as default};
