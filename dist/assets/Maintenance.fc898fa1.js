680111efd8d88f5ead597be481cdfcf86f331d3f
Bratet <ahmedmrabet.002@gmail.com>
2022-12-11 22:52:30 +0100

    adjusting

--- dist/assets/Maintenance.fc898fa1.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index.6bb47fb8.js";function a(){return n("div",{children:"maintenance page"})}export{a as default};
--- dist/assets/Page500.91323b9d.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.6bb47fb8.js";function a(){return e("div",{children:"500 page"})}export{a as default};
