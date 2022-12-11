aee34683d69ee1afdc66cd38d668567bf1fc6eac
Bratet <ahmedmrabet.002@gmail.com>
2022-12-11 23:25:57 +0100

    adding dist

---
+++ dist/assets/ComingSoon.f2110fb2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.21fb1955.js";function i(){return o("div",{children:"coming soon page"})}export{i as default};
---
+++ dist/assets/Maintenance.845336a3.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index.21fb1955.js";function a(){return n("div",{children:"maintenance page"})}export{a as default};
