55c0a2bbb9b02df37ee2f1d3b2b8b34e0ead78d1
Bratet <ahmedmrabet.002@gmail.com>
2022-12-12 00:48:32 +0100

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter >
     </HelmetProvider>
   </React.StrictMode>
 );
