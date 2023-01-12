a6ce6311be96e3ccae25fe81cde62a3e607816a8
Bratet <ahmedmrabet.002@gmail.com>
2023-01-12 03:06:01 +0100

    adding dist

---
+++ assets/ComingSoon.9c812e2a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.bc99c009.js";function i(){return o("div",{children:"coming soon page"})}export{i as default};
---
+++ assets/Page500.6e99b91f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.bc99c009.js";function a(){return e("div",{children:"500 page"})}export{a as default};
