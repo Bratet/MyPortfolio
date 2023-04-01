92e902fe088c90f1abac33078a06d36e9da9d753
Bratet <ahmedmrabet.002@gmail.com>
2023-04-01 23:58:24 +0000

    Initial dist subtree commit

---
+++ assets/Maintenance.4a22e92d.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index.5c7fe767.js";function a(){return n("div",{children:"maintenance page"})}export{a as default};
---
+++ assets/Page500.67dcbc68.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.5c7fe767.js";function a(){return e("div",{children:"500 page"})}export{a as default};
